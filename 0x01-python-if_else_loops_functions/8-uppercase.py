a9bc82daf8f21fbd30490c7108d2dfca18cb6963
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-09 18:03:01 +0100

    fixed requirement error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -10,8 +10,6 @@ def uppercase(str):
     """
 
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
-        else:
-            print("{}".format(i), end="")
+        print("{}".format(chr(ord(i) - 32)
+              if (ord(i) >= 97 and ord(i) <= 122) else (i)))
     print()
