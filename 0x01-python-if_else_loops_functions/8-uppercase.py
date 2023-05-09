489bd3eedf30b9466f00966465cbd32575430cba
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-09 18:09:10 +0100

    added string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -11,5 +11,5 @@ def uppercase(str):
 
     for i in str:
         print("{}".format(chr(ord(i) - 32)
-              if (ord(i) >= 97 and ord(i) <= 122) else (i)))
-    print()
+              if (ord(i) >= 97 and ord(i) <= 122) else (i)), end="")
+    print("".format())
