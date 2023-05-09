c0f9d8a47defab277f7b5afa9d0401b07f235a33
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-09 18:30:57 +0100

    added :c to string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,12 +8,7 @@ def uppercase(str):
     Returns:
         None
     """
-    str_len = len(str)
-    for i in range(str_len):
-        if i == str_len - 1:
-            print("{}".format(chr(ord(str[i]) - 32)
-                  if (ord(str[i]) >= 97 and ord(str[i]) <= 122) else (str[i])))
-        else:
-            print("{}".format(chr(ord(str[i]) - 32)
-                  if (ord(str[i]) >= 97 and ord(str[i]) <= 122)
-                      else (str[i])), end="")
+    for i in str:
+        print("{:c}".format(ord(i) - 32 if (ord(i) >= 97
+              and ord(i) <= 122) else ord(i)), end="")
+    print()
