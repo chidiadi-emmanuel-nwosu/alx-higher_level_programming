e17b867e0a443f286cbdef232ce57212ff344513
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-14 19:16:31 +0100

    fixed error for reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -12,5 +12,6 @@ def print_reversed_list_integer(my_list=[]):
     """
     if not my_list:
         return None
-    for i in my_list.reverse():
+
+    for i in reversed(my_list):
         print("{:d}".format(i))
