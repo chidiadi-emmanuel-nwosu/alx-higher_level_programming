3f89c1306df4b4d83817a895c8b0f9f10ee6a8bb
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-15 21:24:19 +0100

    refactored 13-palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-int check_pal(int *arr, int i, int j);
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -16,39 +15,36 @@ int check_pal(int *arr, int i, int j);
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int i = 0, *arr;
+	listint_t *tmp, *fast, *slow, *prev;
 
-	arr = malloc(sizeof(*arr) * 1);
-	if (!head || !(*head))
+	if (!head || !(*head) || !(*head)->next)
 		return (1);
-	while (tmp)
+
+	fast = *head;
+	slow = *head;
+	tmp = slow;
+	prev = NULL;
+
+	while (fast && fast->next)
 	{
-		arr = realloc(arr, sizeof(*arr) * (i + 1));
-		if (!arr)
-			return (0);
-		arr[i++] = tmp->n;
-		tmp = tmp->next;
+		fast = fast->next->next;
+		slow = slow->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = slow;
 	}
-	return (check_pal(arr, 0, i - 1));
-}
 
+	if (fast != NULL)
+		slow = slow->next;
 
+	while (prev && slow)
+	{
+		if (prev->n != slow->n)
+			return (0);
 
-/**
- * check_pal - checks if a string is palindrome
- * @arr: input integer array
- * @i: start arr index
- * @j: end arr index
- *
- * Return: 1 is s is palindrome, 0 otherwise
- */
+		prev = prev->next;
+		slow = slow->next;
+	}
 
-int check_pal(int *arr, int i, int j)
-{
-	if (arr[i] != arr[j])
-		return (0);
-	if (i >= j)
-		return (1);
-	return (check_pal(arr, ++i, --j));
+	return (1);
 }
