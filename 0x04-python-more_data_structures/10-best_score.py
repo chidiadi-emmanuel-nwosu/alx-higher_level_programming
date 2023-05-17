4bc45c1641c0866045999a8218e902f245b57732
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-17 13:29:50 +0100

    fixed error for 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not len(a_dictionary):
+    if not len(a_dictionary) or a_dictionary is None:
         return None
     return sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)[0][0]
