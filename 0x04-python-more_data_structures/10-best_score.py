3b0bec6658bdb2dd1a8bf2f47bad5111284aa69f
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-17 13:56:14 +0100

    fixed error for 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not len(a_dictionary) or not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or not len(a_dictionary):
         return None
-    return sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)[0][0]
+    max_value = max(a_dictionary.values())
+    for i in a_dictionary.keys():
+        if a_dictionary[i] == max_value:
+            return i
