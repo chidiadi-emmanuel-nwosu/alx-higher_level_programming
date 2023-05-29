8a85d845488a85022c5c5b860533b5279afce29e
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-29 12:37:59 +0100

    fixed error for 4-div

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             return new_list
+        except Exception:
+            ret = 0
         finally:
             new_list.append(ret)
     return new_list
