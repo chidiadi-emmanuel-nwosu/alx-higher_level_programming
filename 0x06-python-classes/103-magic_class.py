50acb204b650f32f5993464502cfdf0d93b84d84
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-05-30 23:50:03 +0100

    fixed rqd error

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 # File: 103-magic_class.py
-# Author: Chidiadi E. Nwosu
-
 import math
 """defines a magic class"""
 
+
 class MagicClass:
     def __init__(self, radius):
+        """initializes a circle"""
         self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-
     def area(self):
+        """find the area of the circle"""
         return self.__radius ** 2 * math.pi
 
-
     def circumference(self):
+        """find the circumference of the circle"""
         return 2 * math.pi * self.__radius
