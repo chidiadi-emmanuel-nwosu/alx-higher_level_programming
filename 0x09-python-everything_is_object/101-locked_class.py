6eec55faadff77444c4a1960712ed9af8d03cc76
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-06 16:43:15 +0100

    added 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""LockedClass module
+"""
+
+
+class LockedClass:
+    """creates a locked class with no attributes
+    """
+
+    def __setattr__(self, name, value):
+        """prevents the user from dynamically creating
+           new instance attributes, except if the new
+           instance attribute is called first_name
+        """
+
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{name}'"
+            )
