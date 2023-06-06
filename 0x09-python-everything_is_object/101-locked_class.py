06815ef5b7aea28092327f3612b694b4fc22a33b
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-06 21:01:22 +0100

    correction made for task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,16 +6,4 @@
 class LockedClass:
     """creates a locked class with no attributes
     """
-
-    def __setattr__(self, name, value):
-        """prevents the user from dynamically creating
-           new instance attributes, except if the new
-           instance attribute is called first_name
-        """
-
-        if name == "first_name" and isinstance(value, str):
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{name}'"
-            )
+    __slots__ = ["first_name"]
