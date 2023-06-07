f3ccb86c34200dd9894132ca1167e125ce0cf12f
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-07 12:23:10 +0100

    fixed 4-square function

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,8 +14,6 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    if size == 0:
-        print()
-
-    for i in range(size):
-        print("#" * size)
+    if size:
+        for i in range(size):
+            print("#" * size)
