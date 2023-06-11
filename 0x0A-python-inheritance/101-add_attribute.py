cd3c883945e5015a67c9913842bb22026e033f48
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-11 20:25:48 +0100

    checked for null input in task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,6 +7,7 @@ def add_attribute(obj, attr, value):
     """adds a new attribute to an object if it’s possible"""
 
     if (
+        not obj or not attr or not value or
         not isinstance(obj, object) or
         isinstance(obj, str) or
         hasattr(obj, attr)
