a189176a904921d614245ebc5b2b462564ee86e1
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-11 20:50:49 +0100

    refactored 101 task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,10 +6,6 @@
 def add_attribute(obj, attr, value):
     """adds a new attribute to an object if it’s possible"""
 
-    if (
-        not isinstance(obj, object) or
-        isinstance(obj, str) #or
-        #hasattr(obj, attr)
-    ):
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attr, value)
