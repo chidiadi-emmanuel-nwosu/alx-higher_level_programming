7f46c27aa7f1d26657a61a4c583951e8206415e9
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-06-11 20:28:11 +0100

    reversed null input for task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,10 +7,9 @@ def add_attribute(obj, attr, value):
     """adds a new attribute to an object if it’s possible"""
 
     if (
-        not obj or not attr or not value or
         not isinstance(obj, object) or
-        isinstance(obj, str) or
-        hasattr(obj, attr)
+        isinstance(obj, str) #or
+        #hasattr(obj, attr)
     ):
         raise TypeError("can't add new attribute")
     setattr(obj, attr, value)
