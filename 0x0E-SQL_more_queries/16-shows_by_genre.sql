bd5b3007069810123cf6695d18584c6aea4c562c
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-07-19 19:06:17 +0100

    capitalised all keywords

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows, and all genres linked to that show,
 -- from the database hbtn_0d_tvshows.
-SELECT ts.title, ifnull(tg.name, 'NULL') AS name
+SELECT ts.title, IFNULL(tg.name, 'NULL') AS name
 FROM tv_shows ts
 LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
 LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
