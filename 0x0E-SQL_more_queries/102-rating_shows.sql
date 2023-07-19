a8f826ecdf1d1af26f9976c876f236d86a61a3c3
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-07-19 20:30:57 +0100

    added advance task 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT ts.title, SUM(tsr.rate) AS rating
+FROM tv_shows ts
+JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY rating DESC;
