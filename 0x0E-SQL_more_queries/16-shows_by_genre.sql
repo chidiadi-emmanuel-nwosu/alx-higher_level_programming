57957638368d27ddeb058d763ab6525779117a4c
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-07-19 19:00:45 +0100

    added task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- script that lists all shows, and all genres linked to that show,
+-- from the database hbtn_0d_tvshows.
+SELECT ts.title, ifnull(tg.name, 'NULL') AS name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
+ORDER BY ts.title;
