3d47610062d69bf9af7cbf89737af3deae3001b5
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-14 13:49:12 +0100

    fixed error for task 5 and 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,9 +7,9 @@ if (argsLength <= 3) {
 } else {
   let args = [];
   for (let i = 2; i < argsLength; i++) {
-    args.push(parseInt(process.argv[i]));
+    args.push(parseInt(process.argv[i], 10));
   }
 
-  args = args.sort().reverse();
+  args = args.sort((a, b) => b - a);
   console.log(args[1]);
 }
