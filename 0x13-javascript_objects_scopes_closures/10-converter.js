e241d4a41e7375a2ee055f178d933f10ba253ebd
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-15 18:37:01 +0100

    fixed error for that 5 and 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (number) {
-    let ret = '';
-    do {
-      ret += (number % base).toString();
-      number = Math.floor(number / base);
-    } while (number > 0);
-    return ([...ret].reverse().join(''));
+    return number.toString(base);
   };
 };
