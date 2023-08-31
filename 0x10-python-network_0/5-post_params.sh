427bd3b6361ab26a16a6847156a3f169ae131869
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-31 13:05:46 +0100

    set total lines to 3

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL,
-# and displays the body of the response
-
+# script that takes in a URL as an argument, sends a GET request to the URL,and displays the body of the response
 curl -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -s "$1"
