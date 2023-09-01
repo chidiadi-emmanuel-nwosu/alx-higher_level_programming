2129ffe2605f81f19e6da32a1b8fe674393e68bd
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-01 15:56:53 +0100

    considered other status codes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    if response.status_code == 200:
+    if response.status_code < 400:
         print(response.text)
     else:
         print("Error code: ", response.status_code)
