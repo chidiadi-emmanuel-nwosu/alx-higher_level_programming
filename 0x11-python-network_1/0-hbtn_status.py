083adcfb9cb551ef72c32b54015b66c516b2c2c3
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-01 15:10:17 +0100

    fixed error for task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ with urllib.request.urlopen(url) as response:
     data = response.read()
 
     print("Body response:")
-    print("\t- type: ", type(data))
-    print("\t- content:", data)
-    print("\t- utf8 content:", data.decode('utf-8'))
+    print("    - type: ", type(data))
+    print("    - content:", data)
+    print("    - utf8 content:", data.decode('utf-8'))
