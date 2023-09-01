19a70f1f12cff9e5d15197d6b70dbb24b19693f2
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-01 15:16:03 +0100

    fixed error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    data = response.read()
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
 
-    print("Body response:")
-    print("\t- type: ", type(data))
-    print("\t- content:", data)
-    print("\t- utf8 content:", data.decode('utf-8'))
+        print("Body response:")
+        print("\t- type: ", type(data))
+        print("\t- content:", data)
+        print("\t- utf8 content:", data.decode('utf-8'))
