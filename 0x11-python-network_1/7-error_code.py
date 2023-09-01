340ce4eb1153b54adee42dc5f5f0480d52ea2662
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-01 16:00:10 +0100

    removed unwanted spaces

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     if response.status_code < 400:
         print(response.text)
     else:
-        print("Error code: ", response.status_code)
+        print("Error code:", response.status_code)
